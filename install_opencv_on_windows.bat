66d22acf738a6a41a47f4a9247dafc894088f657
mango <2321544362@qq.com>
2021-07-12 15:05:35 +0800

    Update install_opencv_on_windows.bat

--- install_opencv_on_windows.bat
+++ install_opencv_on_windows.bat
@@ -22,7 +22,7 @@ mkdir build
 cd build
 ::CMake generate visual studio solution::
 echo "CMake generate visual studio solution..."
-cmake -G "Visual Studio 16 2019" -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ..
+cmake -G "Visual Studio 16 2019" -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 cd ..
 
 :: CMake build the opencv solution
