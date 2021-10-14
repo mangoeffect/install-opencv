51292bdfad1a5afb925d6a236202b25174c630df
mango <2015130324@email.szu.edu.cn>
2021-10-14 14:38:00 +0800

    add test project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿/**
+ * @file main.cpp
+ * @author mango (2321544362@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+ #include <iostream>
+ #include "opencv2/opencv.hpp"
+
+ int main(int argc, char** argv)
+ {
+     std::cout << cv::getBuildInformation() << std::endl;
+     return 0;
+ }
